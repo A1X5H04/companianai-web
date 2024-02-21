f505fddd5482c821f31653d5585ed3a955670aa0
Alish Baig <alishbaig2004@gmail.com>
2024-02-21 17:00:35 +0530

    Update global CSS and layout components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import UIProvider from "@/providers/nextui-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={nunito_sans.className}>
+          <UIProvider>{children}</UIProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
