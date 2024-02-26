bd8e24d6b339911c4326b124bc4bcb42e52d91fa
Alish Baig <alishbaig2004@gmail.com>
2024-02-26 10:54:07 +0530

    Add code to write client certificate to file

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { tmpdir } from "os";
+import fs from "fs";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
+
+fs.writeFile(
+  `${tmpdir()}/server-ca.pem`,
+  process.env.CLIENT_CERTIFICATE!,
+  (err) => {
+    if (err) return console.log(err);
+  }
+);
 
 const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
